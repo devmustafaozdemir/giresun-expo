57c029d4347f238253821c4801c07f455adc6283
Mustafa Özdemir <0mustafaozdemirr@gmail.com>
2026-09-22 18:47:39 +0300

    Header: Ziyaretçi Kayıt butonu yeşil

--- tools/build-pages.mjs
+++ tools/build-pages.mjs
@@ -106,7 +106,7 @@ ${links}
               <svg class="icon btn__icon" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"><path d="M6 22V4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v18Z"/><path d="M6 12H4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h2"/><path d="M18 9h2a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2h-2"/><path d="M10 6h4"/><path d="M10 10h4"/><path d="M10 14h4"/><path d="M10 18h4"/></svg>
               ${t.cta2}
             </a>
-            <a class="btn btn--navy btn--sm" href="${P('register', lang)}">
+            <a class="btn btn--primary btn--sm" href="${P('register', lang)}">
               <svg class="icon btn__icon" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
               ${t.cta}
             </a>
